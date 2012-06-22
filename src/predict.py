1ba125404b7283ce2fbc83c3ddb02b55abc05539
Domas Monkus <domas@monkus.lt>
2012-06-22 23:25:51 +0300

    Sorting.

--- src/predict.py
+++ src/predict.py
@@ -1,8 +1,11 @@
 import csv
 import sys
 import pickle
+import argparse
 
 import matplotlib.pyplot as plt
+
+import predictors
 
 input = csv.reader(open(sys.argv[1], 'rb'), delimiter=',')
 output = csv.writer(open(sys.argv[2], 'wb'), delimiter=',')
@@ -33,26 +36,23 @@ def true_FOF(node):
     FOF = FOF - set(outbound[node])
     return FOF
 
-predicted = []
-predicted_missing = []
-predicted_friends = []
 for row in input:
+    pred = []
     node = int(row[0])
     node_in = set(data['inbound'][node])
     node_out = set(data['outbound'][node])
 
-    diff = list(node_in - node_out)
-    diff_sorted = sorted(diff, key=lambda n: len(data['inbound'][n]))
+    missing_links = predictors.missing_links(node, data)
 
-    predicted_missing.append(len(diff))
+    #true_friends = true_FOF(node)
+    #predicted_friends.append(len(true_friends))
 
-    true_friends = true_FOF(node)
-    predicted_friends.append(len(true_friends))
+    #diff.extend(list(true_friends))
 
-    diff.extend(list(true_friends))
+    pred.extend(missing_links)
 
-    output.writerow([str(node), ' '.join(map(str, diff[:10]))])
-    predicted.append(len(diff))
+    output.writerow([str(node), ' '.join(map(str, pred[:10]))])
+    #predicted.append(len(diff))
 
-plot_histo([predicted, predicted_missing, predicted_friends],
-          ["total", "backlinks", "FOF"])
+#plot_histo([predicted, predicted_missing, predicted_friends],
+#          ["total", "backlinks", "FOF"])
